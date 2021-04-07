8ac8fb73f30d54d6b50f35fd8a49b0421e8a37b9
Giovanni de la Rosa <giovadelarosa@gmail.com>
2021-04-07 17:02:58 -0400

    create super basic home page for deployment purposes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,12 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
-      <div></div>
+    <div className="bg-gray-800 min-h-screen mx-auto">
+      <div>
+        <h1 className="text-gray-300 text-center pt-20 text-6xl md:text-9xl font-bold">
+          ATÚKI
+        </h1>
+      </div>
     </div>
   );
 }
