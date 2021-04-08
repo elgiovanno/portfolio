075edd777cbb42dd62c267b87d726a805b2a279f
Giovanni de la Rosa <giovadelarosa@gmail.com>
2021-04-07 23:33:04 -0400

    move logo to own component

---
+++ src/components/logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <h1 className="text-gray-100 p-6 md:p-10 lg:pl-12 text-xl md:text-4xl font-bold text-center md:text-left">
+      ATÚKI
+    </h1>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
+import Logo from "../components/logo";
+
 export default function Home() {
   return (
-    <div className="bg-gray-800 min-h-screen mx-auto">
+    <div className="bg-gray-700 min-h-screen mx-auto">
       <div>
-        <h1 className="text-gray-300 text-center pt-20 text-6xl md:text-9xl font-bold">
-          ATÚKI
-        </h1>
+        <Logo />
       </div>
     </div>
   );
