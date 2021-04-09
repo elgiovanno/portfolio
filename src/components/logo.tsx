9c634d13f7cb71728afbd4ab625cf7887137feb6
Giovanni de la Rosa <giovadelarosa@gmail.com>
2021-04-09 00:44:03 -0400

    fix lg padding

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,6 +1,6 @@
 export default function Logo() {
   return (
-    <h1 className="text-gray-100 p-6 md:p-10 lg:pl-12 text-xl md:text-4xl font-bold text-center md:text-left">
+    <h1 className="text-gray-100 border-4 inline-block border-t-0 border-l-0 p-6 md:p-10 text-xl md:text-4xl font-bold text-center md:text-left">
       ATÚKI
     </h1>
   );
