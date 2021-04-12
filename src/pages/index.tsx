f040e9ecf89e14337a011d1f90169934cf379f48
Giovanni de la Rosa <giovadelarosa@gmail.com>
2021-04-12 07:17:52 -0400

    extract card class

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,24 +7,35 @@ export default function Home() {
         <Logo />
       </div>
       <div className="border border-blue-500">
-        <div className="px-6 py-12 border border-green-300">
-          <h1 className="mb-4 font-extrabold text-4xl text-gray-800">Who?</h1>
+        <div className="card">
+          <h1 className="card-heading">Who?</h1>
           <p>
             My name is Giovanni de la Rosa. I'm a musician, web developer, and
             like to consider myself of&nbsp;
             <span className="italic">"Student of Life"</span>.
           </p>
-          <br />
+        </div>
+        <div className="card">
+          <h1 className="card-heading">What?</h1>
           <p>
-            What does that even <em>mean</em>?
+            Human, male, son, uncle, friend, multi-instrumentalist, composer,
+            producer, audio engineer, web developer, SysArch enthusiast,
+            surfer/kitesurfer, trailblazer.
           </p>
-          <br />
+        </div>
+        <div className="card">
+          <h1 className="card-heading">When?</h1>
+          <p>August 26th, 1984</p>
+        </div>
+        <div className="card">
+          <h1 className="card-heading">Where?</h1>
+          <p>San Juan, Puerto Rico</p>
+        </div>
+        <div className="card">
+          <h1 className="card-heading">Why?</h1>
           <p>
-            To me, this means having a general curiosity about the people,
-            objects, events and teachings that cross your path. After fighting
-            against the proverbial current of life for so long, I just came to
-            the realization that ALL of the things we encounter have something
-            to teach us.
+            Exposing my work, ideas, thoughts and learnings has never been a
+            strength of mine. This is site an attempt to change that.
           </p>
         </div>
       </div>
