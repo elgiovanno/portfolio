28e4717d3b648531c723e9530520ff73d57102e5
Giovanni de la Rosa <giovadelarosa@gmail.com>
2021-04-12 11:11:46 -0400

    logo update

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
-    <span className="text-gray-900 bg-blue-400 inline-block p-6 text-xl md:text-6xl font-black text-center md:text-left">
-      ATÚKI
+    <span className="text-gray-900 bg-blue-400 inline-block p-6 md:p-10 text-xl md:text-6xl font-black text-center md:text-left">
+      ATÚKI<span className="text-gray-200 border-b-8">.ORG</span>
     </span>
   );
 }
