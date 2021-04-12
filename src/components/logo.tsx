dcd9dd6e50ec678d95e40e74e57f8a903582c573
Giovanni de la Rosa <giovadelarosa@gmail.com>
2021-04-12 09:16:41 -0400

    add some basic visual flare

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,6 +1,6 @@
 export default function Logo() {
   return (
-    <span className="text-gray-900 bg-white inline-block p-6 md:p-10 text-xl md:text-4xl font-black text-center md:text-left">
+    <span className="text-gray-900 bg-blue-400 inline-block p-6 text-xl md:text-6xl font-black text-center md:text-left">
       ATÚKI
     </span>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,13 @@ import Logo from "../components/logo";
 
 export default function Home() {
   return (
-    <div className="bg-gray-100 min-h-screen">
-      <div className="bg-white">
+    <div className="bg-gray-50 min-h-screen">
+      <div className="bg-indigo-400">
         <Logo />
       </div>
-      <div className="">
+      <div className="lg:flex md:justify-center">
         <div className="card">
-          <h1 className="card-heading">Who?</h1>
+          <h1 className="card__heading">Who?</h1>
           <p>
             My name is Giovanni de la Rosa. I'm a musician, web developer, and
             like to consider myself of&nbsp;
@@ -16,7 +16,7 @@ export default function Home() {
           </p>
         </div>
         <div className="card card--white">
-          <h1 className="card-heading">What?</h1>
+          <h1 className="card__heading">What?</h1>
           <p>
             Human, male, son, uncle, friend, multi-instrumentalist, composer,
             producer, audio engineer, web developer, SysArch enthusiast,
@@ -24,15 +24,15 @@ export default function Home() {
           </p>
         </div>
         <div className="card">
-          <h1 className="card-heading">When?</h1>
+          <h1 className="card__heading">When?</h1>
           <p>August 26th, 1984</p>
         </div>
         <div className="card card--white">
-          <h1 className="card-heading">Where?</h1>
+          <h1 className="card__heading">Where?</h1>
           <p>San Juan, Puerto Rico</p>
         </div>
         <div className="card">
-          <h1 className="card-heading">Why?</h1>
+          <h1 className="card__heading">Why?</h1>
           <p>
             Exposing my work, ideas, thoughts and learnings has never been a
             strength of mine. This is site an attempt to change that.
